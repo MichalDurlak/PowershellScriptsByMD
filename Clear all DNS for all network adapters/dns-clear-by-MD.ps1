c636692e2466522a45bfe3bdbfdeb414531fb0dd
Michał Durlak <durlak.m@wp.pl>
2021-10-04 14:47:21 +0200

    #1 - DNS Problem added first script
    Added first script that resolve problem with DNS Cache

---
+++ Clear all DNS for all network adapters/dns-clear-by-MD.ps1
@@ -0,0 +1,13 @@
+﻿$Adapters = Get-NetAdapter
+foreach ($Adapter in $Adapters){
+
+## if ($Adapter.Status -eq "Up"){
+    
+    Set-DnsClientServerAddress -InterfaceIndex $Adapter.ifIndex -ResetServerAddresses
+    Write-Host ("Skasowane")
+    ipconfig /flushdns
+    Write-Host ("Flush Dns zrobione")
+
+## }
+
+}
